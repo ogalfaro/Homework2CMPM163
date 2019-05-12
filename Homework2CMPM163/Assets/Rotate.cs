9977c0d5b1e88249e13c7adc4549e06859edfa45
Oskar Alfaro <ogalfaro@ucsc.edu>
2019-05-12 16:30:52 -0700

    Part B Finished

---
+++ Homework2CMPM163/Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 1; 
+ 
+    void OnMouseDrag(){
+        float rotX = Input.GetAxis("Mouse X")*speed*Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y")*speed*Mathf.Deg2Rad;
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+    }
+}
